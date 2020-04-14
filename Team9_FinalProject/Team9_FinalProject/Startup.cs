adc231b7a34eb7aee3fcaad124623b8c44ecb846
arifeensaeed <47255669+arifeensaeed@users.noreply.github.com>
2020-04-13 21:46:58 -0500

    Update Startup.cs

--- Team9_FinalProject/Team9_FinalProject/Startup.cs
+++ Team9_FinalProject/Team9_FinalProject/Startup.cs
@@ -1,34 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Saeed_Arifeen_HW4.DAL;
+using Saeed_Arifeen_HW4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 
-namespace Team9_FinalProject
+namespace Saeed_Arifeen_HW4
 {
     public class Startup
+
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            String connectionString = "Server=tcp:mis333ksp20.database.windows.net,1433;Initial Catalog=MIS333kSP20-105;Persist Security Info=False;User ID=MIS333kSP20105user;Password=!333Ksp203262;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+            //NOTE: This is where you would change your password requirements
+
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+                { opts.User.RequireUniqueEmail = true;
+                  opts.Password.RequiredLength = 6;
+                  opts.Password.RequireNonAlphanumeric = false;
+                  opts.Password.RequireLowercase = false;
+                  opts.Password.RequireUppercase = false;
+                  opts.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider service)
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
-    }
+    } 
 }
